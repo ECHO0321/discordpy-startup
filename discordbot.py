ba99db8cbbf5ca3befea89489c710fc23c55c9e6
ECHO0321 <63146151+ECHO0321@users.noreply.github.com>
2020-04-05 01:18:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,13 +7,21 @@ import asyncio
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 client = discord.Client()
 
 @client.event
-async def on_ready(startup):
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
     print('------')
-    print('ログインしました！')
-    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("はろー"):
+        m = "こんにちは、" + message.author.name + "さん"
+        await client.send_message(message.channel,m)
 
 client.run('token')
 
